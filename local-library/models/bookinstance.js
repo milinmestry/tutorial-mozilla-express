e580a0b6d8189d6aa5440e8065c0bcfebda15acb
Milin Mestry <milinmestry@gmail.com>
2017-08-30 23:14:19 +0530

    BookInstance list method implemented with moment library for formatting due_back date.

--- local-library/models/bookinstance.js
+++ local-library/models/bookinstance.js
@@ -1,4 +1,6 @@
+var moment = require('moment');
 var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
 var BookInstanceSchema = Schema(
   {
@@ -14,10 +16,18 @@ var BookInstanceSchema = Schema(
 
 // Virtual book url
 BookInstanceSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/bookinstance/' + this._id;
-});
+  .virtual('url')
+  .get(function () {
+    return '/catalog/bookinstance/' + this._id;
+  });
+
+// Virtual formatted Date
+BookInstanceSchema
+  .virtual('due_back_formatted')
+  .get(function () {
+    return moment(this.due_back).format('MMMM Do, YYYY');
+  });
+
 
 // export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);

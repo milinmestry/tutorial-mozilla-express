9ccf19ee7e350c577d81d4bdcab1e99e6bb8815e
Milin Mestry <milinmestry@gmail.com>
2017-08-31 13:09:16 +0530

    Book details page added.

--- local-library/controllers/bookController.js
+++ local-library/controllers/bookController.js
@@ -43,8 +43,27 @@ exports.book_list = function(req, res, next) {
 };
 
 // Display detail page for a specific book
-exports.book_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = function(req, res, next) {
+  async.parallel({
+    book: function(callback) {
+      Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    book_instance: function(callback) {
+      BookInstance.find({'book': req.params.id})
+        // .populate('book')
+        .exec(callback);
+    },
+  }, function(err, results) {
+    if (err) {
+      return next(err);
+    }
+    // Success, render template
+    res.render('book_detail', {title: 'Book Detail'
+      , book: results.book, book_instances: results.book_instance});
+  });
 };
 
 // Display book create form on GET
